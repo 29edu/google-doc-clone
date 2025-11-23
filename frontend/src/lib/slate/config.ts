4a01d9be2949ba0424d0673be158dcbc16c8eb14
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2025-11-23 17:00:18 +0530

    Added the initial value of the document in the config.ts file

---
+++ frontend/src/lib/slate/config.ts
@@ -0,0 +1,12 @@
+import { Descendant } from "slate";
+
+// Initial Value
+
+export const INITIAL_VALUE: Descendant[] = [
+    {
+        type: 'paragraph',
+        children: [
+            {text: 'Start typing your document here ...'}
+        ],
+    },
+]
