2629f158190169a9859431cead3e2f81888621c0
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2025-11-23 17:00:44 +0530

    Updated the App.tsx to show the Initial Ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import Editor from './components/editor/Editor'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Editor />
     </>
   )
 }
