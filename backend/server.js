ff3e2ffd2853b29d25c10186caf8fa0d9713b237
Edison Priyadarshi <edisonpriyadarshi12@gmail.com>
2025-11-27 11:48:53 +0530

    Added routes for userRoutes

--- backend/server.js
+++ backend/server.js
@@ -7,50 +7,9 @@ import User from './src/models/user.models.js';
 const app = express();
 app.use(express.json())
 
-// Test Router - To create a user and see _id
-
-app.post('/test', async (req, res) => {
-    try {
-
-        const email = "edison@1233454";
-
-        let user= await User.findOne({email});
-
-        if(user) {
-            return res.json({
-                success: true,
-                message: "User already exist",
-                userId: user._id,
-                name:user.name
-            })
-        }
-
-        user = await User.create({
-            name: "Edison2",
-            email: "edison@1233456",
-            password: "12345"
-        });
-
-        console.log("Created User", user);
-        console.log("User _id", user._id);
-        console.log("_id type" , typeof user._id);
-        console.log("_id as string", user._id.toString());
-
-        res.json({
-            success: true, 
-            message: "User created",
-            userId: user._id,
-            User: user,
-            name: user.name
-        })
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            error: error.message
-        })
-    }
-    
-})
+// Router
+import userRoutes from './src/routes/userRoutes.js'
+app.use('/api/', userRoutes);
 
 const PORT = process.env.PORT ;
 
